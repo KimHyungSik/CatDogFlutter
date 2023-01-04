9b31a8146cfa8373ab9eaaae5045984332b9d79d
KimHyungSik <colrog968@gmail.com>
2023-01-04 19:35:03 +0900

    Created CarResponse

---
+++ cat_dog/lib/data/model/remote/Cat.dart
@@ -0,0 +1,25 @@
+class CatResponse {
+  String? id;
+  String? imgUrl;
+  int? width;
+  int? height;
+
+  CatResponse({this.id, this.imgUrl, this.width, this.height});
+
+  factory CatResponse.fromJson(Map<String, dynamic> json) {
+    return CatResponse(
+        id: json["id"],
+        imgUrl: json["url"],
+        width: json["width"],
+        height: json["height"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['url'] = imgUrl;
+    map['width'] = width;
+    map['height'] = height;
+    return map;
+  }
+}
