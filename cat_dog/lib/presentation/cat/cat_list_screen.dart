e8465155da87329603afd43d56628fc7e7b4a1ba
KimHyungSik <colrog968@gmail.com>
2023-01-16 22:51:09 +0900

    Added GetIt Dependencies

--- cat_dog/lib/presentation/cat/cat_list_screen.dart
+++ cat_dog/lib/presentation/cat/cat_list_screen.dart
@@ -1,13 +1,6 @@
-import 'package:cat_dog/data/model/remote/Cat.dart';
-import 'package:cat_dog/data/remote/service/cat_service.dart';
-import 'package:cat_dog/domain/model/remote/CatImage.dart';
-import 'package:cat_dog/domain/repository/cat_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
 
-import '../../data/repository/cat_repository_imp.dart';
 import 'cat_provider.dart';
 
 class CatListScreen extends StatelessWidget {
